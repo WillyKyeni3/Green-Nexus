7d2eb760ce8c1ae97902c9dffccefddce2125921
Terry Yegon <yegonterry618@gmail.com>
2025-10-23 00:10:52 +0300

    Add UserProfile component with basic structure

---
+++ client/src/app/UserProfile/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold">User Profile</h1>
+            {/* Add user profile details here */}
+        </div>
+    );
+};
+
+export default UserProfile;
