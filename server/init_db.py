96a8885ebc1fb738932bde6b257b4ca9a5f86a5f
Willy Kyeni <willykyeni@gmail.com>
2025-10-28 18:00:05 +0300

    updates to recreate thedatabse

--- server/init_db.py
+++ server/init_db.py
@@ -1,4 +1,3 @@
-#server/init_db.py
 import os
 from app import create_app, db
 
@@ -9,4 +8,4 @@ app = create_app() # Create an app instance using your factory
 
 with app.app_context(): #Push an application context
     db.create_all() #Create all tables defined by your models
-    print("Database tables created successfully.")
+    print("Database tables created/updated successfully.")
