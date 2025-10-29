f2bd91cf3095c4c21d63c9ac8fa42e6d5791830c
Shamola Shelton <shamolashelton@gmail.com>
2025-10-29 15:03:11 +0300

    adds render.yaml

--- server/build.sh
+++ server/build.sh
@@ -2,10 +2,5 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies for Pillow
-apt-get update
-apt-get install -y libjpeg-dev zlib1g-dev
-
-# Upgrade pip and install Python packages
 pip install --upgrade pip
 pip install -r requirements.txt
