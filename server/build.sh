28324249e7b4ddba4e703f957f9b0fe2a71f1caa
Shamola Shelton <shamolashelton@gmail.com>
2025-10-29 14:56:24 +0300

    updates build.sh

--- server/build.sh
+++ server/build.sh
@@ -1,6 +1,11 @@
-bash#!/usr/bin/env bash
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
+# Install system dependencies for Pillow
+apt-get update
+apt-get install -y libjpeg-dev zlib1g-dev
+
+# Upgrade pip and install Python packages
 pip install --upgrade pip
 pip install -r requirements.txt
